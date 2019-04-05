4b628e0308a72d7580f9fbdc1a09d640038c7331
Jimmy Zebroski <zebrozkii@gmail.com>
2019-04-05 09:41:08 -0700

    changed a file name causing me to misspell a selector tag realized that and now things are working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Shoe } from './models/shoe.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/nike-shoes/nike-shoes.component.ts
+++ src/app/nike-shoes/nike-shoes.component.ts
@@ -1,17 +1,16 @@
 import { Component} from '@angular/core';
-
+import { Shoe } from '../models/shoe.model';
 @Component({
   selector: 'app-nike-shoes',
   templateUrl: './nike-shoes.component.html',
   styleUrls: ['./nike-shoes.component.css']
 })
 export class NikeShoesComponent {
-  // nike: Shoe[] = [
-  //   new Shoe("Jordan",200);
-  //   new Shoe("AirMax",120);
-  //   new Shoe("Nike Reacts", 160);
-  //   new Shoe("Nike Sb" 110);
-  //
-  // ]
+  nike: Shoe[] = [
+    new Shoe("Jordan",200),
+    new Shoe("AirMax",120),
+    new Shoe("Nike Reacts", 160),
+    new Shoe("Nike Sb" ,110),
+  ];
 
 }
