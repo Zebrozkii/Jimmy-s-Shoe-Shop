0dd61e625cffa822a5d7d229a8da5c32bcac9f09
Jimmy Zebroski <zebrozkii@gmail.com>
2019-04-05 11:00:37 -0700

    adding the cart component now which will let people select shows and put them in a different list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NikeShoesComponent } from './nike-shoes/nike-shoes.component';
 import { AdidasShoesComponent } from './adidas-shoes/adidas-shoes.component';
 import { VansShoesComponent } from './vans-shoes/vans-shoes.component';
 import { GucciShoesComponent } from './gucci-shoes/gucci-shoes.component';
+import { YourCartComponent } from './your-cart/your-cart.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { GucciShoesComponent } from './gucci-shoes/gucci-shoes.component';
     NikeShoesComponent,
     AdidasShoesComponent,
     VansShoesComponent,
-    GucciShoesComponent
+    GucciShoesComponent,
+    YourCartComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/your-cart/your-cart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Shoe } from '../models/shoe.model';
+@Component({
+  selector: 'app-your-cart',
+  templateUrl: './your-cart.component.html',
+  styleUrls: ['./your-cart.component.css']
+})
+export class YourCartComponent {
+
+
+}
